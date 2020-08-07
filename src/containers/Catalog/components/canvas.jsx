fded3371751d1469f2a8a4e3075f9e30a58357c3
NikitaKompanetc <kni2012@ukr.net>
2020-08-07 14:52:20 +0400

    render, color work

--- src/containers/Catalog/components/canvas.jsx
+++ src/containers/Catalog/components/canvas.jsx
@@ -1,67 +1,65 @@
-import React, { Component, useEffect, useState } from 'react';
+import React, { Component, useEffect, useState} from 'react';
 import { fabric } from 'fabric';
 import '../../../scss/component/canvas.scss';
 import tShirt from '../../../img/Womens-T-Shirt-Example.png';
+import { render } from '@testing-library/react';
 
 
-const CanvasPreview = (props) => {
-  console.log(17, props.colorProduct)
-
-
-useEffect( () => {
-
-  var canvas = new fabric.Canvas('c');
-        var image;
-        var imgEl = document.createElement('img');
-        imgEl.crossOrigin = 'anonymous';
-        imgEl.onload = function() {
-          image = new fabric.Image(imgEl);
-          image.set({  
-                left: 150,
-                top: 50,
-                }).scale(1);
-          image.hasControls = false;
-          image.hasBorders = false;
-          image.lockMovementX = true;
-          image.lockMovementY = true;
-          image.lockRotation = true;
-          image.scaleToHeight(300);
-          image.scaleToWidth(300);
-          canvas.add(image);
-
-          var textbox = new fabric.Textbox('Lorum ipsum dolor sit amet', {
-            left: 100,
-            top: 50,
-            width: 150,
-            fontSize: 20,
-            fill: '#ff0000'
-          });
-            canvas.add(textbox);
-            canvas.requestRenderAll();
-        }
-        //imgEl.src = 'https://i.imgur.com/28kU1bo.png';
-        //imgEl.src = 'https://i.imgur.com/4VGs2y9.png';
-        imgEl.src = tShirt;
-        
-        // document.getElementById('hue').onclick = function() {
-          image.filters.push(new fabric.Image.filters.BlendColor({
-                color: props.colorProduct, // make it red!  props.colorProduct
-                mode: 'overlay',
-                alpha: 0.6 
-              }))
-          image.applyFilters();
-          canvas.add(image);
-          canvas.requestRenderAll();
-        // };
-
-        // test = () => {
-        //   canvas.requestRenderAll();
-        //   var dataURL = canvas.toDataURL({format: 'jpeg',})
-        //     console.log(dataURL)
-        // }
+class CanvasPreview extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = { 
       
-      canvas.requestRenderAll();
-})
+    }
+    this.canvasRender = this.canvasRender.bind(this);
+}
+
+componentDidMount() {
+//   var canvas = new fabric.Canvas('c');
+//   var image;
+//   var imgEl = document.createElement('img');
+//   imgEl.crossOrigin = 'anonymous';
+//   imgEl.onload = function() {
+//     image = new fabric.Image(imgEl);
+//     image.set({  
+//           left: 150,
+//           top: 50,
+//           }).scale(1);
+//     image.hasControls = false;
+//     image.hasBorders = false;
+//     image.lockMovementX = true;
+//     image.lockMovementY = true;
+//     image.lockRotation = true;
+//     image.scaleToHeight(300);
+//     image.scaleToWidth(300);
+//     canvas.add(image);
+
+//     var textbox = new fabric.Textbox('Lorum ipsum dolor sit amet', {
+//       left: 100,
+//       top: 50,
+//       width: 150,
+//       fontSize: 20,
+//       fill: '#ff0000'
+//     });
+//       canvas.add(textbox);
+//       canvas.requestRenderAll();
+//   }
+//   //imgEl.src = 'https://i.imgur.com/28kU1bo.png';
+//   //imgEl.src = 'https://i.imgur.com/4VGs2y9.png';
+//   imgEl.src = tShirt;
+  
+//   document.getElementById('hue').onclick = function() {
+//     image.filters.push(new fabric.Image.filters.BlendColor({
+//           color: 'red', // make it red!  props.colorProduct
+//           mode: 'overlay',
+//           alpha: 0.6 
+//         }))
+//     image.applyFilters();
+//     canvas.add(image);
+//     canvas.requestRenderAll();
+//   };  
+// canvas.requestRenderAll();
 
 
 
@@ -113,19 +111,83 @@ useEffect( () => {
     // elem.addEventListener("mouseup", function(){
     //   console.log(115)
     // })
-
-
-return (
-        <div>
-          <button id="hue">Change to random color</button>
-          {/* <button onClick={() => zoomin()}>click</button> */}
-          <div className='canvas-containers'>
-            <canvas id="c"  width={650} height={350}></canvas>
-          </div>
-        </div>
-  )
+    this.canvasRender()
 
 }
+canvasRender(){
+  console.log(118, this.props)
+  let canvas = new fabric.Canvas('c');
+  var image;
+  var imgEl = document.createElement('img');
+  imgEl.crossOrigin = 'anonymous';
+  imgEl.onload = function() {
+    image = new fabric.Image(imgEl);
+    image.set({  
+          left: 150,
+          top: 50,
+          }).scale(1);
+    image.hasControls = false;
+    image.hasBorders = false;
+    image.lockMovementX = true;
+    image.lockMovementY = true;
+    image.lockRotation = true;
+    image.scaleToHeight(300);
+    image.scaleToWidth(300);
+    canvas.add(image);
+
+    var textbox = new fabric.Textbox('Lorum ipsum dolor sit amet', {
+      left: 100,
+      top: 50,
+      width: 150,
+      fontSize: 20,
+      fill: '#ff0000'
+    });
+      canvas.add(textbox);
+      canvas.requestRenderAll();
+  }
+  //imgEl.src = 'https://i.imgur.com/28kU1bo.png';
+  //imgEl.src = 'https://i.imgur.com/4VGs2y9.png';
+  imgEl.src = tShirt;
+  
+  document.getElementById('hue').onclick = function() {
+    image.filters.push(new fabric.Image.filters.BlendColor({
+          color: 'red', // make it red!  props.colorProduct
+          mode: 'overlay',
+          alpha: 0.6 
+        }))
+    image.applyFilters();
+    canvas.add(image);
+    canvas.requestRenderAll();
+  };  
+canvas.requestRenderAll();
+
+document.getElementById('setRender').onclick = function() {
+  canvas.requestRenderAll();
+  var dataURL = canvas.toDataURL({format: 'jpeg',})
+    console.log(dataURL)
+  }
+}
+
+
+
+
+  
+
+    render() {
+
+      return (
+              <div>
+                <button id="hue">Change to random color</button>
+                <button id='setRender'>Render</button>
+                <button onClick={this.canvasRender}>click</button>
+                <div className='canvas-containers'>
+                  <canvas id="c"  width={650} height={350}></canvas>
+                </div>
+              </div>
+        );
+      };
+
+};
 
 export default CanvasPreview;
 
